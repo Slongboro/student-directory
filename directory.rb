2d3c1d36f9ba24b55fe00711d692e2f6d0bb0c86
jtsalva <tim@jtsalva.dev>
2019-10-03 14:37:29 +0100

    refactor cleaner code now

--- directory.rb
+++ directory.rb
@@ -1,43 +1,52 @@
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
 
 def interactive_menu
-  students = []
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
+    print_menu
     # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    process(gets.chomp)
   end
 end
 
 #first we print the list of students
 def input_students
-  puts "Please enter the names of the students"
   puts " To finish, just hit return twice"
-  students = []
+  puts "Please enter the names of the students"
+
   name = gets.chomp
   while !name.empty? do
     puts "Enter cohort"
     cohort = gets.chomp
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 
 end
 
@@ -46,10 +55,10 @@ def print_header
   puts "--------------"
 end
 
-def print(names)
+def print_students_list
   puts "Please give me a letter"
   letter = gets.chomp
-  names.each.with_index do |student, index|
+  @students.each.with_index do |student, index|
     if student[:name].downcase.start_with?(letter) && student[:name].length < 12
       puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
     end
@@ -58,16 +67,16 @@ def print(names)
 end
 
 #finally, we print the total number of students
-def print_footer(names)
-  if names.count == 1
-  puts "Overall, we have #{names.count} great student"
+def print_footer
+  if @students.count == 1
+  puts "Overall, we have #{@students.count} great student"
 else
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 end
 
-def sort_by_cohort(students)
-  students.sort_by{
+def sort_by_cohort
+  @students.sort_by{
     |student|
     student[:cohort] }
 end
